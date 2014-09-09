63f46e3810cb036347718d842b640561357af686
Allen Madsen <blatyo@gmail.com>
2014-09-08 21:38:09 -0400

    Clean up git repo handling

--- spec/support/context/local_git_repo.rb
+++ spec/support/context/local_git_repo.rb
@@ -1,14 +1,15 @@
 require 'rugged'
 
 shared_context 'local git repo' do
-  let(:repositories_path){File.expand_path('../../../../tmp/repos', __FILE__)}
-  let(:repo_path){File.join(repositories_path, 'test')}
-  let(:repo) do
-    build(:git_repo, :bad_ruby_file)
+  let(:repo_path){repo.path.sub('/.git/', '')}
+  let(:repo){build(:git_repo, :bad_ruby_file)}
+  let(:diff){build(:git_diff, repo: repo)}
+  let(:patch){build(:git_patch, diff: diff)}
+  let(:delta){build(:git_delta, patch: patch)}
+  let(:hunk){build(:git_hunk, patch: patch)}
+  let(:line){build(:git_line, hunk: hunk)}
+
+  after(:each) do
+    FileUtils.rm_rf(repo_path, {})
   end
-  let(:diff){build(:git_diff)}
-  let(:patch){build(:git_patch)}
-  let(:delta){build(:git_delta)}
-  let(:hunk){build(:git_hunk)}
-  let(:line){build(:git_line)}
 end
